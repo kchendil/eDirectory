c613ec160ac22dbed90d515eb4e026caa411872a
kchendil <mkchendil@gmail.com>
2013-04-26 11:40:55 -0400

    edirectory configureation
    yet to test

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: idm
+# Cookbook Name:: edirectory
 # Attributes:: default
 #
 # Copyright 2010, Opscode, Inc.
@@ -19,5 +19,17 @@
 
 
 default['edirectory']['edirectory_build_loc'] = "/home/vagrant/IDM402AE/test/products/eDirectory/x64/setup/nds-install"
+default['edirectory']['edirectory_install_loc'] = "/opt/novell/eDirectory"
+default['edirectory']['edirectory_tree_name'] = "VAGRANT"
+default['edirectory']['edirectory_dib_loc'] = "/var/opt/novell/eDirectory"
+default['edirectory']['edirectory_admin']="admin"
+default['edirectory']['edirectory_context']="servers.system"
+default['edirectory']['edirectory_ldap_clear_port']="389"
+default['edirectory']['edirectory_ldap_ssl_port']="636"
+default['edirectory']['edirectory_port']="524"
+default['edirectory']['edirectory_imonitor_http_port']="8028"
+default['edirectory']['edirectory_imonitor_https_port']="8030"
+default['edirectory']['edirectory_password']="n"
+default['edirectory']['edirectory_conf_file']="/etc/opt/novell/eDirectory/conf/nds.conf"
 
 
